e4d2be6f55ce510f58e2107c3f5013e5625df1c5
Benoit Hiller <benoit.hiller@gmail.com>
2014-11-03 21:20:17 -0500

    removing duplicate config reload logic

--- lib/utility.sh
+++ lib/utility.sh
@@ -190,3 +190,41 @@ getIRCInfo() {
   echo "${infoArray[@]}"
   return 0
 }
+
+declare watchedFunctionName
+declare watchedFunctionPid
+
+# Helper to manage the saved function.
+#
+# Only call this from startAndWatch.
+startFunction() {
+  if [ -n "$watchedFunctionPid" ]; then
+    killtree "$watchedFunctionPid"
+    wait "$watchedFunctionPid"
+  fi
+
+  "$watchedFunctionName" <&0 &
+  local -r newPid="$!"
+  watchedFunctionPid="$newPid"
+}
+
+# Runs the specified function restarting it when SIGHUP is recieved
+#
+# Only run this once per subshell as multiple instances will clobber each other.
+# This function will also wait forever when started.
+#
+# 1. the name of the function to run. Assigned to global variable.
+startAndWatch() {
+  watchedFunctionName="$1"
+
+  startFunction
+
+  trap startFunction SIGHUP 
+
+  while true; do
+    if [ -n "$watchedFunctionPid" ]; then
+      wait $watchedFunctionPid
+    fi
+    sleep 1
+  done
+}
