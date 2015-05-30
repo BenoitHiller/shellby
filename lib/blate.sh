a0a3d440c7eb53a1553d5f346d497e220f5f27ef
Benoit Hiller <benoit.hiller@gmail.com>
2015-05-30 11:58:40 -0400

    adding template language

---
+++ lib/blate.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+e() {
+  local -r joined="$@"
+  printf "%s" "$joined"
+}
+
+p() {
+  printf "$@"
+}
+
+# split the template file
+#
+# separates the file into alternating segments of not code, then code
+#
+# 1.file the file to split
+split() {
+  local -r file="$1"
+  
+  awk '
+    BEGIN {
+      RS="{{|}}"
+      last=""
+    }
+    {
+      if ((RT == "{{" && $0 ~ /^{/) || (RT == "}}" && $0 ~ /^}/) ) {
+        last += $0;
+      } else {
+        if (NR > 1) {
+          printf("%s\0", last);
+        }
+        last = $0
+      }
+    }
+    END {
+      printf("%s\0", last);
+    }
+  ' "$file"
+}
+
+# evaluate the output of split
+#
+# evaluates every second line and echos the rest
+evaluate() {
+  local raw=true
+  local template="$(
+    while read -d $'\0' line; do
+      if $raw; then
+        raw=false
+        printf 'printf "%s" %q ;\n' "$line"
+      else
+        raw=true
+        if [[ -n "$line" ]]; then
+          printf '%s ;\n' "$line"
+        fi
+      fi
+    done)"
+  eval "$template"
+}
+
+# Render a template file
+#
+# 1.file the file to render
+render() {
+  local -r file="$1"
+
+  split "$file" | evaluate
+}
